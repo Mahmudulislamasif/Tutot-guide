278f38ec9e0d3bf182066975f9d22ff8e911c92d
Mahmudulislamasif <mahmudulislamasif2000@gmail.com>
2022-04-17 14:56:59 +0600

    third question add

--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -16,6 +16,25 @@ const Blogs = () => {
                    <p>There are many data storage platforms are used to stored user data firebase is one of them. I mainly used firebase for it's security. Also, There are many advantages firebase have. Some advantages are: fast web hosting, Multi-Platfoorm Authentication, Unlimited Google Analytics for mobile apps etc. Other Options for authentication are MongoDB,AUth0,Amazon Cognito,Okta, Passport, JSON web token etc. </p>
                  </div>
               </div>
+              <div className='col-md-6'>
+                 <div className='blogs-class shadow-lg mt-3'>
+                   <h5>What other services does firebase provide other than authentication</h5>
+                   <p>Firebase is not only confined with authentication but it also provide many services other than authentication.Facilities provide by firebase is given below.
+                    <ol type='1'>
+                    <li>
+                        Cloud Messaging: That sends notification to Mobile
+                    </li>
+                    <li>
+                        Hosting: It is able to host one's website by providing link.
+                    </li>
+                    <li>
+                        Realtime database: Store and sync app data in miliseconds.
+                    </li>
+                    </ol>
+
+                   </p>
+                 </div>
+              </div>
            </div>
         </div>
     );
