4f76968040b58071d2c49b5e9c8f09860302622b
Mahmudulislamasif <mahmudulislamasif2000@gmail.com>
2022-04-17 15:07:44 +0600

    add signout button

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,7 +1,15 @@
+import { signOut } from 'firebase/auth';
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
+import auth from '../../../firebase.init';
 import './Header.css'
 const Header = () => {
+    const [user]=useAuthState(auth)
+    const handleSignOut=()=>
+    {
+        signOut(auth)
+    }
     return (
         <div>
            <div className='bg-color'>
@@ -16,7 +24,10 @@ const Header = () => {
                    <Link className='link-class' to='/about'>About</Link>
                </div>
                <div className='col-md-1 ms-auto'>
-                   <Link className='link-class' to='/login'>Login</Link>
+                   {
+                       user? <Link className='link-class' onClick={handleSignOut} to='/login'>Signout</Link>:
+                       <Link className='link-class' to='/login'>Login</Link>
+                   }
                </div>
 
             </div>
